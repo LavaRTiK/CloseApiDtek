c2177cdcbd9786bec4d337b0c9503190457a0d74
LavaRT <pavlo.legominecraft@gmail.com>
2024-12-08 21:15:31 +0200

    need test period

--- Svitlo/Component/TrackingAddress.cs
+++ Svitlo/Component/TrackingAddress.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
@@ -32,17 +33,39 @@ namespace Svitlo.Component
             notifyIcon = new NotifyIcon();
             notifyIcon.Text = $"Svitlo ({followingObject.Name})";
             notifyIcon.Icon = new Icon("molnia.ico");
-            notifyIcon.Visible = false;
+            notifyIcon.Visible = true;
             dataLoderAPI = new DataLoderAPI();
         }
         public void StartFollowing()
         {
-            SetTimerUpdate();
-            Task.Run(async () =>
+            Task.Run(async ()  =>
             {
+                SetTimerUpdate();
                 while (!cancellationToken.IsCancellationRequested)
                 {
+                    TimeOnly currentTime = TimeOnly.FromDateTime(DateTime.Now);
+                    TimeOnly time = new TimeOnly(22, 00);
+                    TimeOnly testOne = new TimeOnly(1, 00);
+                    TimeOnly testTwo = new TimeOnly(0, 15);
+                    TimeOnly testThree = new TimeOnly(0, 5);
 
+                    for (int i = 0; i < currentDicssonect.Count; i++) 
+                    {
+                        if(currentDicssonect[currentTime.AddHours(1)] == "+" || currentDicssonect[currentTime.AddHours(1)] == "+-")
+                        {
+                            notifyIcon.ShowBalloonTip(2000, $"Svitlo ({followingObject.Name})", "Відключення світла через годину", ToolTipIcon.Info);
+                        }
+                        else if(currentDicssonect[currentTime.AddMinutes(15)] == "+" || currentDicssonect[currentTime.AddMinutes(15)] == "+-")
+                        {
+                            notifyIcon.ShowBalloonTip(2000, $"Svitlo ({followingObject.Name})", "Відключення світла через 15 хвилин", ToolTipIcon.Info);
+                        }
+                        else if (currentDicssonect[currentTime.AddMinutes(5)] == "+" || currentDicssonect[currentTime.AddMinutes(5)] == "+-")
+                        {
+                            notifyIcon.ShowBalloonTip(2000, $"Svitlo ({followingObject.Name})", "Відключення світла через 5 хвилин", ToolTipIcon.Info);
+                        }
+                    }
+                    await Task.Delay(1000);
+                    
                 }
                 aTimer.Stop();
                 aTimer.Dispose();
@@ -56,14 +79,11 @@ namespace Svitlo.Component
         }
         private async void UpdateData(Object source, ElapsedEventArgs e)
         {
-            MessageBox.Show("тут начало");
             var temp = await dataLoderAPI.RequestDisconnectDataAsync(followingObject.City, followingObject.IdCity, followingObject.Street, followingObject.IdStreet, followingObject.House, followingObject.IdHouse);
             if (temp != null)
             {
-                MessageBox.Show("тут");
-                if(currentData != temp[2].ToString())
+                if(currentData != temp[2].data.ToString())
                 {
-                    MessageBox.Show("зашол");
                     currentData = temp[2].data.ToString();
                     ParsingHTMLDoccument();
                     notifyIcon.ShowBalloonTip(2000,$"Svitlo ({followingObject.Name})","Данні оновлено",ToolTipIcon.Info);
