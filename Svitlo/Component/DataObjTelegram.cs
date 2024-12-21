cd42143656372f0276773c5e9484e9a4a02316b3
LavaRT <pavlo.legominecraft@gmail.com>
2024-12-21 22:48:00 +0200

    Add DataObjTelegram

---
+++ Svitlo/Component/DataObjTelegram.cs
@@ -0,0 +1,57 @@
+﻿using Svitlo.ObjectModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Svitlo.Component
+{
+    public class DataObjTelegram
+    {
+        private static TelegramObj telegramObj = new TelegramObj();
+        public async Task ReadDataAsync()
+        {
+            if (!File.Exists("telegram.txt"))
+            {
+                File.Create("telegram.txt").Close();
+            }
+            using (StreamReader sr = new StreamReader("telegram.txt"))
+            {
+                string data = await sr.ReadToEndAsync();
+                if (!string.IsNullOrWhiteSpace(data))
+                {
+                    try
+                    {
+                        telegramObj = JsonSerializer.Deserialize<TelegramObj>(data);
+                    }
+                    catch (Exception ex)
+                    {
+                        telegramObj = new TelegramObj();
+                    }
+                }
+                else
+                {
+                    telegramObj = new TelegramObj();
+                }
+            }
+        }
+        public async Task LoadDataAsync()
+        {
+            if (!File.Exists("telegram.txt"))
+            {
+                File.Create("telegram.txt").Close();
+            }
+            using (StreamWriter sw = new StreamWriter("telegram.txt"))
+            {
+                var options = new JsonSerializerOptions()
+                {
+                    WriteIndented = true,
+                };
+                string data = JsonSerializer.Serialize(telegramObj, options);
+                await sw.WriteAsync(data);  
+            }
+        }
+    }
+}
